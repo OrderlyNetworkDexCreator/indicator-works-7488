1753eeeba5f05121fae4bb64d51afb7d7d3636bd
Mario Reder <mario.reder@pm.me>
2026-01-16 14:06:31 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading",
-  "VITE_CUSTOM_MENUS": "Create, https://indicator.works/create; Router, https://indicator.works/router; Pricing, https://indicator.works/pricing",
+  "VITE_CUSTOM_MENUS": "Create, https://indicator.works/create; Switch, https://indicator.works/switch; Pricing, https://indicator.works/pricing",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
